7f2083a9be0b2128d209ae71b63b84ff32c94be6
Telmo Sampaio <telmoasampaio@gmail.com>
2021-02-21 19:18:40 +0000

    Adding frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import Product from './pages/Product'
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/result" component={Result}/>
+        <Route exact path="/:productId" component={Product}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
