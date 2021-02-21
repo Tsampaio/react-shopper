0ad9d942a00b2479838a0a94e80a744a91de07cb
Telmo Sampaio <telmoasampaio@gmail.com>
2021-02-21 20:06:33 +0000

    React QUery

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import Home from './pages/Home';
 import Result from './pages/Result';
 import Product from './pages/Product'
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/result" component={Result}/>
-        <Route exact path="/:productId" component={Product}/>
-      </Switch>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/result" component={Result}/>
+          <Route exact path="/:productId" component={Product}/>
+        </Switch>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useQuery } from 'react-query';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 export default function ProductList() {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    axios.get('/api/products')
-    .then(res => res.data.products)
-    .then(products => setProducts(products))
+  const { data: products, isLoading } = useQuery('Products', () => axios.get('/api/products')
+  .then(res => res.data.products))
 
-  }, [])
+  if(isLoading) return <LoadingSpinner />
+
   return products.map(product => (
     <ProductItem key={product.id} product={product} />
   ))
