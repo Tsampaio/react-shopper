8f22624099ab7d3c2f058abed9e3801c3c0d4408
Telmo Sampaio <telmoasampaio@gmail.com>
2021-03-27 17:49:49 +0000

    Add modal to show cart

--- client/src/components/CartModal.js
+++ client/src/components/CartModal.js
@@ -1,18 +1,26 @@
 // @ts-nocheck
 import React from "react";
 import Modal from "react-modal";
+import { useShoppingCart } from "use-shopping-cart";
 
 Modal.setAppElement("#root");
 
-export default function CartModal() {
+export default function CartModal({isOpen, toggleModal}) {
+  const {cartCount, formattedTotalPrice} = useShoppingCart()
+
   return (
-    <Modal contentLabel="Cart Modal" closeTimeoutMS={500}>
+    <Modal 
+      isOpen={isOpen}
+      onRequestClose={toggleModal}
+      contentLabel="Cart Modal" 
+      closeTimeoutMS={500}
+    >
       <div className="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-gray-800">
         <div className="bg-white rounded-lg w-80 sm:w-1/2 md:w-2/3 lg:w-1/2">
           <div className="flex flex-col items-start p-4 full m">
             <div className="flex items-center w-full mb-4">
               <div className="text-gray-900 font-medium text-lg">
-                Cart Summary: Total Price (Cart Count Items)
+                Cart Summary: {formattedTotalPrice} ({cartCount} items)
               </div>
             </div>
             <hr />
@@ -23,7 +31,10 @@ export default function CartModal() {
               >
                 Checkout Now
               </button>
-              <button className="bg-transparent hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+              <button 
+                className="bg-transparent hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                onClick={toggleModal}
+              >
                 Still Shopping
               </button>
             </div>
--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { useShoppingCart } from "use-shopping-cart";
+import CartModal from "./CartModal";
 import { CartIcon } from "./Icons";
 
 export default function CartSummary() {
   const { formattedTotalPrice, cartCount } = useShoppingCart();
+  const [isOpen, setOpen] = React.useState(false);
+
+  const toggleModal = () => setOpen(!isOpen);
 
   return (
     <>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav onClick={toggleModal} className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <span className="mr-5 hover:text-white flex items-center">
           <CartIcon />
           <span className="ml-3">{formattedTotalPrice} ({cartCount})</span>
         </span>
       </nav>
+      <CartModal isOpen={isOpen} toggleModal={toggleModal} />
     </>
   );
 }
